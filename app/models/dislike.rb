431a283d48212d2c7f25ce9d17e8e5022124b091
Mark <mark.n.bacon.jr@gmail.com>
2022-05-29 16:18:19 -0500

    omg does it work?
    This reverts commit d9bbe5051674ada4f2e4b90fbcb06434b7b2aba1.

--- app/models/dislike.rb
+++
@@ -1,7 +0,0 @@
-class Dislike < ActiveRecord::Base
-    #this user can have many liked users
-    belongs_to :disliker, class_name: "User"
-    belongs_to :disliked, class_name: "User"
-
-        
-end
--- db/migrate/20220401062240_create_dislikes.rb
+++
@@ -1,8 +0,0 @@
-class CreateDislikes < ActiveRecord::Migration[6.1]
-  def change
-    create_table :dislikes do |t|
-      t.integer :disliked_id 
-      t.integer :disliker_id
-    end 
-  end
-end
